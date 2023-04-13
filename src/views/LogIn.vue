db43c2ea0a3aeb60bbe84cbfdab77edc37e8d7f6
OwenOmar998 <owenabuhamour@gmail.com>
2023-04-13 12:04:27 +0300

    Editing Form

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -3,24 +3,40 @@
     <!-- Login form -->
     <div class="login" v-if="fireBase.showSignInForm">
       <h1>Login</h1>
-      <form @submit.prevent="fireBase.signin(signInEmail, signInPassword)">
+      <form @submit.prevent="fireBase.signin(email, password)">
         <div class="txt_field">
-          <input type="text" required="true" v-model="signInEmail" />
+          <input
+            id="email"
+            name="email"
+            type="text"
+            required="true"
+            v-model.trim="email"
+            @blur="validateEmail"
+          />
           <span></span>
-          <label>Username</label>
+          <label for="email">Email</label>
         </div>
+        <h5 class="error" v-if="emailValidity === 'invalid'">
+          Please enter a valid email!
+        </h5>
         <div class="txt_field">
-          <input type="password" required="true" v-model="signInPassword" />
+          <input
+            id="password"
+            name="password"
+            type="password"
+            required="true"
+            v-model.trim="password"
+          />
           <span></span>
-          <label>Password</label>
+          <label for="password">Password</label>
         </div>
         <div class="submitBtn">
           <p class="text-danger" v-if="!fireBase.signInValid">
             {{ fireBase.errorMsg }}
           </p>
           <button
-            :disabled="fireBase.loginReq"
-            :id="fireBase.loginReq ? 'disabled' : ''"
+            :disabled="fireBase.loginReq || !submitButton"
+            :id="fireBase.loginReq || !submitButton ? 'disabled' : ''"
             type="submit"
           >
             <span v-if="!fireBase.loginReq">Login</span>
@@ -52,37 +68,77 @@
     <div class="signup" v-else>
       <h1>Sign up</h1>
       <form
-        @submit.prevent="
-          fireBase.signUp(firstName, lastName, signUpEmail, signUpPassword)
-        "
+        @submit.prevent="fireBase.signUp(firstName, lastName, email, password)"
       >
         <div class="txt_field">
-          <input type="text" required="true" v-model="firstName" />
+          <input
+            id="first-name"
+            name="first-name"
+            type="text"
+            required="true"
+            v-model.trim="firstName"
+            @blur="validateFirstName"
+          />
           <span></span>
-          <label>First Name</label>
+          <label for="firest-name">First Name</label>
         </div>
+        <h5 class="error" v-if="firstNameValidity === 'invalid'">
+          Please enter a valid name
+        </h5>
         <div class="txt_field">
-          <input type="text" required="true" v-model="lastName" />
+          <input
+            id="last-name"
+            name="last-name"
+            type="text"
+            required="true"
+            v-model.trim="lastName"
+            @blur="validateLastName"
+          />
           <span></span>
           <label>Last Name</label>
         </div>
+        <h5 class="error" v-if="lastNameValidity === 'invalid'">
+          Please enter a valid name
+        </h5>
         <div class="txt_field">
-          <input type="text" required="true" v-model="signUpEmail" />
+          <input
+            id="email"
+            name="email"
+            type="text"
+            required="true"
+            v-model.trim="email"
+            @blur="validateEmail"
+          />
           <span></span>
-          <label>Username</label>
+          <label for="email">Email</label>
         </div>
+        <h5 class="error" v-if="emailValidity === 'invalid'">
+          Please enter a valid email
+        </h5>
         <div class="txt_field">
-          <input type="password" required="true" v-model="signUpPassword" />
+          <input
+            id="password"
+            name="password"
+            type="password"
+            required="true"
+            v-model.trim="password"
+            @blur="validatePassword"
+          />
           <span></span>
-          <label>Password</label>
+          <label for="password">Password</label>
         </div>
+        <h5 class="error" v-if="passwordValidity === 'invalid'">
+          Password must be at least 8 characters long and contain at least one
+          uppercase letter, one lowercase letter, one number, and one special
+          character.
+        </h5>
         <div class="submitBtn">
           <p class="text-danger" v-if="!fireBase.signUpValid">
             {{ fireBase.errorMsg }}
           </p>
           <button
-            :disabled="fireBase.signUpReq"
-            :id="fireBase.signUpReq ? 'disabled' : ''"
+            :disabled="fireBase.signUpReq || !submitButton"
+            :id="fireBase.signUpReq || !submitButton ? 'disabled' : ''"
             type="submit"
           >
             <span v-if="!fireBase.signUpReq">Sign up</span>
@@ -99,48 +155,89 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, watch } from "vue";
+import { defineComponent, onMounted, ref, watch } from "vue";
 import { useFireBase } from "../stores/FireBase";
 import router from "@/router";
+import { computed } from "@vue/reactivity";
 
 export default defineComponent({
   name: "logIn",
   setup() {
     const fireBase = useFireBase();
 
-    const signInEmail = "",
-      signInPassword = "",
-      signUpEmail = "",
-      signUpPassword = "",
-      firstName = "",
-      lastName = "";
-
-    watch(
-      () => fireBase.user,
-      (newValue, oldValue) => {
-        if (newValue) router.push("/");
-      }
-    );
+    const email = ref(""),
+      password = ref(""),
+      firstName = ref(""),
+      lastName = ref(""),
+      emailValidity = ref("pending"),
+      firstNameValidity = ref("pending"),
+      lastNameValidity = ref("pending"),
+      passwordValidity = ref("pending");
+    const submitButton = computed(() => {
+      if (fireBase.showSignInForm)
+        return emailValidity.value === "valid" && password.value.length
+          ? true
+          : false;
+      else
+        return emailValidity.value === "valid" &&
+          passwordValidity.value === "valid" &&
+          firstNameValidity.value === "valid" &&
+          lastNameValidity.value === "valid"
+          ? true
+          : false;
+    });
 
     return {
-      signInEmail,
-      signInPassword,
-      signUpEmail,
-      signUpPassword,
+      email,
+      password,
       firstName,
       lastName,
       fireBase,
+      emailValidity,
+      passwordValidity,
+      firstNameValidity,
+      lastNameValidity,
+      submitButton,
     };
   },
   methods: {
+    validateFirstName() {
+      const regex = /^[a-zA-Z]{1,}$/;
+      regex.test(this.firstName)
+        ? (this.firstNameValidity = "valid")
+        : (this.firstNameValidity = "invalid");
+    },
+    validateLastName() {
+      const regex = /^[a-zA-Z]{1,}$/;
+      regex.test(this.lastName)
+        ? (this.lastNameValidity = "valid")
+        : (this.lastNameValidity = "invalid");
+    },
+    validateEmail() {
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      regex.test(this.email)
+        ? (this.emailValidity = "valid")
+        : (this.emailValidity = "invalid");
+    },
+    validatePassword() {
+      const regex =
+        /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      regex.test(this.password)
+        ? (this.passwordValidity = "valid")
+        : (this.passwordValidity = "invalid");
+      console.log(this.password);
+    },
     toggleForm() {
       this.fireBase.toggleForm();
-      this.signInEmail = "";
-      this.signInPassword = "";
-      this.signUpEmail = "";
-      this.signUpPassword = "";
+      this.email = "";
+      this.password = "";
       this.firstName = "";
       this.lastName = "";
+      this.emailValidity = "pending";
+      this.firstNameValidity = "pending";
+      this.lastNameValidity = "pending";
+      this.passwordValidity = "pending";
+      this.submitButton = false;
     },
   },
   components: {},
